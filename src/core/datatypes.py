c241776fda53f84f5056c3906944775e9231a0aa
Yu <ge75qeb@mytum.de>
2026-08-14 13:57:32 +0900

    feat: add pipeline state

--- src/core/datatypes.py
+++ src/core/datatypes.py
@@ -1,5 +1,6 @@
+import numpy as np
 from dataclasses import dataclass
-
+from dataclasses import dataclass, field
 @dataclass
 class Detection:
   bbox: tuple[float, float, float, float]
@@ -13,5 +14,8 @@ class Detection:
     x1,y1,x2,y2 = self.bbox
     return (x1+x2) / 2 , (y1+y2) / 2
 
-d = Detection((10, 20, 50, 80), 0.87, 2, "car", 14)
-print(d.center)
+@dataclass
+class Pipleinestate:
+  frame_id:int
+  image: np.ndarry
+  detections: list[Detection] = field(default_factory=list)
