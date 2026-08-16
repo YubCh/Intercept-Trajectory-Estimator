8246d04fabd4183b2a7b608a90b7496b605f08ed
Yu <ge75qeb@mytum.de>
2026-08-16 18:21:08 +0900

    feat: add process, finish

--- src/data_scan/recorder.py
+++ src/data_scan/recorder.py
@@ -2,7 +2,7 @@ import csv
 from pathlib import Path
 from src.core.base_module import BaseModule
 
-class CvsRecorder(BaseModule):
+class CsvRecorder(BaseModule):
   def __init__(self, output_dir="results"):
     self.output_dir = Path(output_dir)
     self.output_dir.mkdir(parents=True,exist_ok=True)
@@ -15,3 +15,21 @@ class CvsRecorder(BaseModule):
 
     self.predictions_writer.writerow(["frame_id", "track_id", "horizon", "pred_x", "pred_y"])
     self.positions_writer.writerow(["frame_id", "track_id", "x", "y"])
+
+  def process(self, state):
+    for track in state.tracks:
+      x,y = track.last_detection.center
+      self.positions_writer.writerow([state.frame_id,track.track_id,x,y])
+
+    for prediction in state.predictions:
+      self.predictions_writer.writerow([
+      prediction.frame_id,
+      prediction.track_id,
+      prediction.horizon,
+      prediction.point[0],
+      prediction.point[1],
+      ])
+
+  def finish(self):
+    self.predictions_file.close()
+    self.positions_file.close()
